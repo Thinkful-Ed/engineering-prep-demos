d1bc6cebf2df433894e283b91167455bb426be81
Chris Klanac <cklanac@users.noreply.github.com>
2019-07-09 13:21:18 -0600

    Prep demo code for demo

--- application-logic/app.js
+++ application-logic/app.js
@@ -1,29 +1,31 @@
 // original solution
 function isLeapYear(year) {
-    if ( year % 400 === 0 ) {
-        return true;
-    }
+  if (year % 400 === 0) {
+    return true;
+  }
 
-    else if ( year % 100 === 0 ) {
-        return false;
-    }
+  else if (year % 100 === 0) {
+    return false;
+  }
 
-    else if ( year % 4 === 0 ) {
-        return true;
-    }
+  else if (year % 4 === 0) {
+    return true;
+  }
 
-    else {
-        return false;
-    }
+  else {
+    return false;
+  }
 }
 
 // alternate solution
+/* 
 function isLeapYear(year) {
     if ( (year % 400 === 0) || ((year % 100 !== 0) && (year % 4 === 0)) ) {
         return true;
     } 
     return false;
-}
+} 
+*/
 
 console.log(isLeapYear(1700));
 console.log(isLeapYear(1990));
